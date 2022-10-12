bd58f5ff23ea2c97b5277a5bd92c6e80da0e1e76
alejandro-blockbites <alejandro.saez@defdone.com>
2022-10-12 20:35:19 +0200

    Installed yargs to get arguments. Multiple repos

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,14 @@
+#!/usr/bin/env node
 const { Octokit } = require("octokit");
 const dotenv = require("dotenv");
+const yargs = require("yargs");
 
-//Repo to check
-const owner = "gitkraken";
-const repo = "vscode-gitlens";
+const options = yargs.usage("Usage: -r <repo URL>").option("r", {
+  alias: "repo",
+  describe: "Repo URL to get Stats",
+  type: "string",
+  demandOption: true,
+}).argv;
 
 // Configuration the .env file
 dotenv.config();
@@ -14,11 +19,8 @@ const octokit = new Octokit({
   auth: token,
 });
 
-async function getRepo() {
-  const repoInfo = await octokit.request("GET /repos/{owner}/{repo}", {
-    owner: owner,
-    repo: repo,
-  });
+async function getRepo(repo) {
+  const repoInfo = await octokit.request("GET /repos/" + repo);
   if (repoInfo.status !== 200) {
     const message = `An error has occured: ${repoInfo.status}`;
     throw new Error(message);
@@ -26,13 +28,9 @@ async function getRepo() {
   return repoInfo.data;
 }
 
-async function getContributors() {
+async function getContributors(repo) {
   const repoContributors = await octokit.request(
-    "GET /repos/{owner}/{repo}/stats/contributors",
-    {
-      owner: owner,
-      repo: repo,
-    }
+    "GET /repos/" + repo + "/stats/contributors"
   );
   if (repoContributors.status !== 200) {
     const message = `An error has occured: ${repoContributors.status}`;
@@ -41,13 +39,9 @@ async function getContributors() {
   return repoContributors.data;
 }
 
-async function getPulls() {
+async function getPulls(repo) {
   const pullsInfo = await octokit.request(
-    "GET /repos/{owner}/{repo}/pulls?state=open&per_page=100",
-    {
-      owner: owner,
-      repo: repo,
-    }
+    "GET /repos/" + repo + "/pulls?state=open&per_page=100"
   );
   if (pullsInfo.status !== 200) {
     const message = `An error has occured: ${pullsInfo.status}`;
@@ -56,13 +50,9 @@ async function getPulls() {
   return pullsInfo.data;
 }
 
-async function getBranches() {
+async function getBranches(repo) {
   const branchesInfo = await octokit.request(
-    "GET /repos/{owner}/{repo}/branches?per_page=100",
-    {
-      owner: owner,
-      repo: repo,
-    }
+    "GET /repos/" + repo + "/branches?per_page=100"
   );
   if (branchesInfo.status !== 200) {
     const message = `An error has occured: ${branchesInfo.status}`;
@@ -71,59 +61,68 @@ async function getBranches() {
   return branchesInfo.data;
 }
 
-//Number of: pr, forks, stars, closed/open issues, dominant language, license,last commit date
-getRepo()
-  .then(function (result) {
-    console.log("-----General Information-----");
-    console.log("Repository: " + result.name);
-    console.log(
-      "Created: " +
-        result.created_at.split("T")[0] +
-        " at " +
-        result.created_at.split("T")[1].split("Z")[0]
-    );
-    console.log(
-      "Updated: " +
-        result.updated_at.split("T")[0] +
-        " at " +
-        result.updated_at.split("T")[1].split("Z")[0]
-    );
-    console.log(
-      "Pushed: " +
-        result.pushed_at.split("T")[0] +
-        " at " +
-        result.pushed_at.split("T")[1].split("Z")[0]
-    );
-    console.log("Dominant Language: " + result.language);
-    console.log("License: " + result.license.name);
-    console.log("Forks: " + result.forks);
-    console.log("Stars: " + result.stargazers_count);
-    const openIssues = result.open_issues;
+//Get args
+const repos = options.repo.split(",");
+repos.forEach((repoListed) => {
+  const repo = repoListed.split("https://github.com/")[1];
+  //Number of: pr, forks, stars, closed/open issues, dominant language, license,last commit date
+  getRepo(repo)
+    .then(function (result) {
+      console.log("Repository: " + result.name);
+      console.log(
+        "Created: " +
+          result.created_at.split("T")[0] +
+          " at " +
+          result.created_at.split("T")[1].split("Z")[0]
+      );
+      console.log(
+        "Updated: " +
+          result.updated_at.split("T")[0] +
+          " at " +
+          result.updated_at.split("T")[1].split("Z")[0]
+      );
+      console.log(
+        "Pushed: " +
+          result.pushed_at.split("T")[0] +
+          " at " +
+          result.pushed_at.split("T")[1].split("Z")[0]
+      );
+      console.log("Dominant Language: " + result.language);
+      if (result.license !== null) {
+        console.log("License: " + result.license.name);
+      } else {
+        console.log("No License");
+      }
 
-    //Separate PR from Issues
-    getPulls().then(function (result) {
-      console.log("Open PR: " + result.length);
-      console.log("Open Issues: " + (openIssues - result.length));
+      console.log("Forks: " + result.forks);
+      console.log("Stars: " + result.stargazers_count);
+      const openIssues = result.open_issues;
+
+      //Separate PR from Issues
+      getPulls(repo).then(function (result) {
+        console.log("Open PR: " + result.length);
+        console.log("Open Issues: " + (openIssues - result.length));
+      });
+    })
+    .catch((error) => {
+      console.log(`[FETCHING ERROR]:Not Available: ${error}`);
     });
-  })
-  .catch((error) => {
-    console.log(`[FETCHING ERROR]:Not Available: ${error}`);
-  });
 
-//Show contributors
-getContributors()
-  .then((result) => {
-    console.log("Contributors: " + result.length);
-  })
-  .catch((error) => {
-    console.log(`[FETCHING ERROR]:Not Available: ${error}`);
-  });
+  //Show contributors
+  getContributors(repo)
+    .then((result) => {
+      console.log("Contributors: " + result.length);
+    })
+    .catch((error) => {
+      console.log(`[FETCHING ERROR]:Not Available: ${error}`);
+    });
 
-//Show branches
-getBranches()
-  .then((result) => {
-    console.log("Branches: " + result.length);
-  })
-  .catch((error) => {
-    console.log(`[FETCHING ERROR]:Not Available: ${error}`);
-  });
+  //Show branches
+  getBranches(repo)
+    .then((result) => {
+      console.log("Branches: " + result.length);
+    })
+    .catch((error) => {
+      console.log(`[FETCHING ERROR]:Not Available: ${error}`);
+    });
+});
