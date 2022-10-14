2ef868b3609ae30881935eb8d4087173e359d231
alejandro-blockbites <alejandro.saez@defdone.com>
2022-10-14 11:34:48 +0200

    Added repos from file option -f

--- bin/index.js
+++ bin/index.js
@@ -6,13 +6,19 @@ const fs = require("fs");
 
 const options = yargs
   .usage(
-    "Usage: -r <URLs of all the repositories to analyse. Comma separated.>"
+    "Usage: \r\n-r <URLs of all the repositories to analyse. Comma separated.> \r\n -f <File with URLs of all the repositories to analyse. Comma separated.>"
   )
   .option("r", {
     alias: "repo",
     describe: "Repo URL to get Stats",
     type: "string",
-    demandOption: true,
+    demandOption: false,
+  })
+  .option("f", {
+    alias: "repoFile",
+    describe: "File name containing  Repo URLs to get Stats",
+    type: "string",
+    demandOption: false,
   })
   .option("v", {
     alias: "verbose",
@@ -24,6 +30,11 @@ const options = yargs
 // Configuration the .env file
 dotenv.config();
 const token = process.env.TOKEN || "";
+if (token === "") {
+  console.log(
+    "[INFO] Search done without Github Token. Could be some limitations.\r\n To get better results crete file .env and add Github secret token: TOKEN=ghp_XX... "
+  );
+}
 
 //Configure Octokit with token
 const octokit = new Octokit({
@@ -74,7 +85,17 @@ async function getRepo(ownerRepo) {
 }
 
 //Get args
-const repos = options.repo.split(",");
+let repos = "";
+if (options.repo !== "" && options.repo !== undefined) {
+  repos = options.repo.split(",");
+} else if (options.repoFile !== "" && options.repoFile !== undefined) {
+  //Get info from file
+  var text = fs.readFileSync(options.repoFile, "utf8");
+  repos = text.toString().split(",");
+} else {
+  console.log("Needed repo to analyse");
+  process.exit();
+}
 
 const csv = [
   "name",
@@ -95,79 +116,83 @@ const fileName = Date.now().toString() + ".csv";
 fs.writeFileSync(fileName, csv);
 repos.forEach((repoListed) => {
   const repo = repoListed.split("https://github.com/")[1];
-  getRepo(repo).then(
-    ([
-      repoInfo,
-      repoContributors,
-      pullsInfo,
-      branchesInfo,
-      closedIssues,
-      closedPulls,
-    ]) => {
-      if (options.verbose) {
-        console.log("Repository: " + repoInfo.name);
-        console.log(
-          "Created: " +
-            repoInfo.created_at.split("T")[0] +
-            " at " +
-            repoInfo.created_at.split("T")[1].split("Z")[0]
-        );
-        console.log(
-          "Updated: " +
-            repoInfo.updated_at.split("T")[0] +
-            " at " +
-            repoInfo.updated_at.split("T")[1].split("Z")[0]
-        );
-        console.log(
-          "Pushed: " +
-            repoInfo.pushed_at.split("T")[0] +
-            " at " +
-            repoInfo.pushed_at.split("T")[1].split("Z")[0]
-        );
-        console.log("Dominant Language: " + repoInfo.language);
+  getRepo(repo)
+    .then(
+      ([
+        repoInfo,
+        repoContributors,
+        pullsInfo,
+        branchesInfo,
+        closedIssues,
+        closedPulls,
+      ]) => {
+        if (options.verbose) {
+          console.log("Repository: " + repoInfo.name);
+          console.log(
+            "Created: " +
+              repoInfo.created_at.split("T")[0] +
+              " at " +
+              repoInfo.created_at.split("T")[1].split("Z")[0]
+          );
+          console.log(
+            "Updated: " +
+              repoInfo.updated_at.split("T")[0] +
+              " at " +
+              repoInfo.updated_at.split("T")[1].split("Z")[0]
+          );
+          console.log(
+            "Pushed: " +
+              repoInfo.pushed_at.split("T")[0] +
+              " at " +
+              repoInfo.pushed_at.split("T")[1].split("Z")[0]
+          );
+          console.log("Dominant Language: " + repoInfo.language);
 
-        if (repoInfo.license !== null) {
-          console.log("License: " + repoInfo.license.name);
-        } else {
-          console.log("No License");
+          if (repoInfo.license !== null) {
+            console.log("License: " + repoInfo.license.name);
+          } else {
+            console.log("No License");
+          }
+          console.log("Forks: " + repoInfo.forks);
+          console.log("Stars: " + repoInfo.stargazers_count);
+          //Separate PR from Issues
+          console.log("Open PR: " + pullsInfo.length);
+          console.log("Closed PR: " + closedPulls.total_count);
+          console.log(
+            "Open Issues: " + (repoInfo.open_issues - pullsInfo.length)
+          );
+          console.log("Closed Issues: " + closedIssues.total_count);
+          //Show contributors
+          console.log("Contributors: " + repoContributors.length);
+          //Show branches
+          console.log("Branches: " + branchesInfo.length);
         }
-        console.log("Forks: " + repoInfo.forks);
-        console.log("Stars: " + repoInfo.stargazers_count);
-        //Separate PR from Issues
-        console.log("Open PR: " + pullsInfo.length);
-        console.log("Closed PR: " + closedPulls.total_count);
-        console.log(
-          "Open Issues: " + (repoInfo.open_issues - pullsInfo.length)
-        );
-        console.log("Closed Issues: " + closedIssues.total_count);
-        //Show contributors
-        console.log("Contributors: " + repoContributors.length);
-        //Show branches
-        console.log("Branches: " + branchesInfo.length);
+        //create csv
+        let licenseName = "No License";
+        if (repoInfo.license !== null) {
+          licenseName = repoInfo.license.name;
+        }
+        const row =
+          "\r\n" +
+          [
+            repoInfo.name,
+            pullsInfo.length,
+            closedPulls.total_count,
+            repoInfo.forks,
+            repoInfo.stargazers_count,
+            repoInfo.open_issues - pullsInfo.length,
+            closedIssues.total_count,
+            repoInfo.language,
+            licenseName,
+            repoContributors.length,
+            repoInfo.created_at.split("T")[0],
+            repoInfo.updated_at.split("T")[0],
+            repoInfo.pushed_at.split("T")[0],
+          ].join(",");
+        fs.appendFileSync(fileName, row);
       }
-      //create csv
-      let licenseName = "No License";
-      if (repoInfo.license !== null) {
-        licenseName = repoInfo.license.name;
-      }
-      const row =
-        "\r\n" +
-        [
-          repoInfo.name,
-          pullsInfo.length,
-          closedPulls.total_count,
-          repoInfo.forks,
-          repoInfo.stargazers_count,
-          repoInfo.open_issues - pullsInfo.length,
-          closedIssues.total_count,
-          repoInfo.language,
-          licenseName,
-          repoContributors.length,
-          repoInfo.created_at.split("T")[0],
-          repoInfo.updated_at.split("T")[0],
-          repoInfo.pushed_at.split("T")[0],
-        ].join(",");
-      fs.appendFileSync(fileName, row);
-    }
-  );
+    )
+    .catch((error) => {
+      LogError(`[ERROR]: Repo ${repo} not analysed -> ${error}`);
+    });
 });
